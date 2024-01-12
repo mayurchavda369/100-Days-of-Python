09a3c2b439ece80941c1f43eb25618ee81ac6a6e
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-01-12 15:08:32 +0530

    Day 1
    What is python ? its use and features?

---
+++ Day 1.py
@@ -0,0 +1,72 @@
+Introduction with Python
+Python is a high-level, interpreted programming language that emphasizes code readability and simplicity. 
+It was developed by Guido van Rossum and first released in 1991.
+ Python is an interpreted, object-oriented, high-level programming language with 
+dynamic semantics. 
+ Its high-level built in data structures, combined with dynamic typing and dynamic 
+binding, make it very attractive for Rapid Application Development.
+ Python supports modules and packages, which encourages program modularity and 
+code reuse. 
+ The Python interpreter and the extensive standard library are available in source or 
+binary form without charge for all major platforms, and can be freely distributed.
+Why Python?
+ Designed to be easy to learn and master
+ Clean, clear syntax
+ Very few keywords
+ Highly portable
+ Runs almost anywhere - high end servers and workstations, down to 
+windows CE
+ Uses machine independent byte-code
+ Extensible
+ Designed to be extensible using C/C++, 
+ allowing access to many external libraries
+Features of Python
+ Clean syntax plus high-level data types
+ Leads to fast coding (First language in many universities abroad!)
+ Uses white-space to delimit blocks
+ Humans generally do, so why not the language?
+ Try it, you will end up liking it
+ Variables do not need declaration
+ Although not a type-less language
+Python Productivity
+ Reduced development time
+ code is 2-10x shorter than C, C++, Java
+ Improved program maintenance
+ code is extremely readable
+ Less training
+ language is very easy to learn
+OOPS in Python
+ Python has been an object-oriented language since it existed. Because of this, 
+creating and using classes and objects are downright easy.
+ Overview of OOP Terminology
+ Class: A user-defined prototype for an object that defines a set of attributes that 
+characterize any object of the class. The attributes are data members (class variables 
+and instance variables) and methods, accessed via dot notation.
+ Class variable: A variable that is shared by all instances of a class. Class variables are 
+defined within a class but outside any of the class's methods. Class variables are not 
+used as frequently as instance variables are.
+ Data member: A class variable or instance variable that holds data associated with a 
+class and its objects.
+ Function overloading: The assignment of more than one behavior to a particular 
+function. The operation performed varies by the types of objects or arguments 
+involved.
+ Instance variable: A variable that is defined inside a method and belongs only to the 
+current instance of a class.
+ Inheritance: The transfer of the characteristics of a class to other classes that are 
+derived from it.
+ Instance: An individual object of a certain class. An object obj that belongs to a class 
+Circle, for example, is an instance of the class Circle.
+ Instantiation: The creation of an instance of a class.
+ Method: A special kind of function that is defined in a class definition.
+ Object: A unique instance of a data structure that's defined by its class. An object 
+comprises both data members (class variables and instance variables) and methods.
+ Operator overloading: The assignment of more than one function to a particular 
+operator.
+Programming Style
+ Python programs/modules are written as text files with traditionally a .py extension.
+ Each Python module has its own discrete namespace.
+ Name space within a Python module is a global one.
+ Python modules and programs are differentiated only by the way they are called.
+ .py files executed directly are programs (often referred to as scripts)
+ .py files referenced via the import statement are modules.
+ Thus, the same .py file can be a program/script, or a module.
