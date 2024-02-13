5b1085443cf35600eff229767c3165f9a67e4e36
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-02-13 15:45:02 +0530

    Function in Python
    What is Function  in Python ? types and use.

---
+++ Day 16.py
@@ -0,0 +1,14 @@
+# Function in Python:
+# a function is a block of organized, reusable code that performs a specific task. 
+# Functions provide better modularity and code reusability in programming. 
+# They allow you to break down your program into smaller, manageable pieces, making your code easier to understand and maintain.
+# There are Three types of Function in Python:
+# 1. Built in Function
+# 2. User defined Function (define by user, "def" keyword using)
+# 3. Lambda or Anonymous Function. its also called as single line function.(use lambda keyward) lambda arguments: expression
+
+# Built in function in Python.
+# Python has a rich standard library that provides numerous built-in functions to perform various operations. 
+# As of Python 3.10, there are over 70 built-in functions available. Some of the most commonly used built-in functions include 
+# print(), len(), range(),input(), str(), int(), float(), list(), dict(), type(), sum(), max(), min(), abs(), round(),sorted(), zip(), map(), filter(), 
+# reduce(), enumerate(),any(), all(), open(), format(), dir(), help(), callable(), isinstance(), issubclass(), eval(), exec(), and globals().
