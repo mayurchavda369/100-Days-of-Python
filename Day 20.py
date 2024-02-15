220702cf9b3ca20e0cfe86097275867bc7547d98
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-02-15 15:57:23 +0530

    Examples of lambda function
    Top programs of lambda function.

---
+++ Day 20.py
@@ -0,0 +1,58 @@
+# Example of Lambda Function: you can write single line program with lambda function.
+# Ex 1: sum of two number
+# sum=lambda a,b:a+b
+# x=sum(100,20)
+# print(x)
+# o/p-> 120
+
+# Ex 2: Not deversable value with 2
+# res=(lambda r:r%2!=0)(80)
+# print(res)
+# o/p-> False
+
+# Ex 3: square of any number
+# square=lambda x:x*x
+# y=square(16)
+# print(y)
+# o/p-> 256
+
+# Ex 4: Cube of any number
+# cube=lambda x: x*x*x
+# z=cube(5)
+# print(z)
+# o/p->125
+
+# Ex 5: Average of two numbers
+# Avg=lambda x,y:(x+y)/2
+# m=Avg(12,18)
+# print(m)
+# o/p-> 15.0
+
+# Ex 6: pass the function into function.
+# def fun1(a,b):
+#     return 6 +a(b)
+
+# cube=lambda x : x*x*x
+
+# print(fun1(cube,2))
+# o/p-> 14 
+
+# Ex 7 : Reverse the string
+# n="mayur"
+# reverse_string = lambda s: s[::-1]
+# print(reverse_string(n))  
+# o/p->ruyam
+
+# Ex 8: Getting the First Character of a String:
+# first_char = lambda s: s[0]
+# print(first_char("hello"))
+# Output: "h"
+
+
+
+
+
+
+
+
+
