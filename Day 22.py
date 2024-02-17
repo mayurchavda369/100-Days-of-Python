a0fe2bcf682fcccfc0a9f42f2d7e02fe1bbcc052
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-02-17 16:08:53 +0530

    Errors in Python
    Errors in python.

---
+++ Day 22.py
@@ -0,0 +1,23 @@
+# Errors in Python:
+# Major 10 types Error in python.
+# Syntax Errors: Syntax errors, also known as parsing errors, occur when the code is not syntactically valid Python code. These errors are detected by the Python parser during the parsing phase, before the code is executed.
+
+# Indentation Errors: Indentation errors occur when the indentation of the code is incorrect. In Python, indentation is used to define the structure of the code (e.g., for loops, if statements). Indentation errors typically occur when there is a mix of tabs and spaces or when the indentation is inconsistent.
+
+# Name Errors: Name errors occur when the Python interpreter encounters a name that it doesn't recognize. This can happen if you try to use a variable or function that hasn't been defined, or if you misspell a variable or function name.
+
+# Type Errors: Type errors occur when an operation is performed on objects of incompatible types. For example, trying to add a string and an integer will result in a type error.
+
+# Value Errors: Value errors occur when a function receives an argument with the correct type but an inappropriate value. For example, trying to convert a string to an integer when the string doesn't represent a valid integer will result in a value error.
+
+# Index Errors: Index errors occur when you try to access an index that is outside the bounds of a sequence (e.g., a list, tuple, or string).
+
+# Key Errors: Key errors occur when you try to access a key that doesn't exist in a dictionary.
+
+# Attribute Errors: Attribute errors occur when you try to access an attribute or method that doesn't exist for a given object.
+
+# File Not Found Errors: File not found errors occur when you try to open a file that doesn't exist.
+
+# Import Errors: Import errors occur when you try to import a module that doesn't exist or when there is an error in the import statement.
+
+# These are some of the common types of errors in Python, but there are many other specific error types that can occur depending on the context of your code. Understanding these error types and how to handle them is important for writing robust and reliable Python code.
