688611e42f8085501a9cecd4614a918645f0fabe
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-02-19 17:39:33 +0530

    Password generator(random,string module)
    Password generator using module

---
+++ Day 29.py
@@ -0,0 +1,22 @@
+# Password generator using Random and String module:
+# import random
+# import string
+
+# # Define the characters to choose from for generating the password
+# characters = string.ascii_letters + string.digits + string.punctuation
+
+# # Generate a random password of length 12
+# password = ''.join(random.choice(characters) for _ in range(12))
+
+# print("Generated Password:", password)
+
+# Using Def function:
+# import random
+# import string
+
+# def password():
+#     characters=string.ascii_letters + string.digits + string.punctuation
+#     password=''.join(random.choice(characters) for _ in range(12))
+#     print("Your Password is : ",password)
+# password()
+# o/p-> Your Password is :  9J=SthT,{]3f
