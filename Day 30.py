300820c615b450300c90ee0221bb2418e95660bc
mayurchavda369 <130274117+mayurchavda369@users.noreply.github.com>
2024-02-19 17:49:55 +0530

    Calender using calender and datetime module
    calender and datetime modules

---
+++ Day 30.py
@@ -0,0 +1,22 @@
+# Calender using calender module:
+# from calendar import *
+# year=int(input("Enter Year: "))
+# print(calendar(year,2,1,8,3))
+# # 2=2 character for days (mo,tu etc)
+# # 1=1 line(row) for each week
+# # 8=8(row) for each month
+# # 3=3(coloumns) for all months of the year.
+# o/p-> input is 1996 print the 1996 full calender.
+
+# print todays date using datetime module:
+# import datetime
+
+# # Get today's date
+# today = datetime.date.today()
+
+# # Print today's date
+# print("Today's date:", today)
+# o/p-> Today's date: 2024-02-19
+
+
+
